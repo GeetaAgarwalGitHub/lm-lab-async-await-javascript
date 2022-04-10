93b45317a93537721bf8d62b4f64a2c8fac5d12b
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-11 00:32:22 +0100

    Update exercise002.js
    Activity 2

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,24 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
+const fetchData = async (apiEndPoint) => {
   fetch(apiEndPoint)
     .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log(error));
 };
 
+const fetchData1 = async (apiEndPoint) => {
+  try {
+    const asyncResponse = await fetch(apiEndPoint);
+    const jsonResponse = await asyncResponse.json();
+    console.log("Yay!Async Fetch successful with response: ");
+    console.log(jsonResponse);
+  } catch (error) {
+    console.log("Boo. Async Fetch failed with response ");
+    console.log(error);
+  }
+};
+
 fetchData(jsonTypicode);
+fetchData1(jsonTypicode);
